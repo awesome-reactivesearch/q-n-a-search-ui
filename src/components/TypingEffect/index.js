e5da3ce64d260ee1794b9c424fd78a49b968d980
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-03-16 12:35:36 +0530

    add TypingEffect component

---
+++ src/components/TypingEffect/index.js
@@ -0,0 +1,47 @@
+import "./typingeffect.css";
+import React, { useState, useEffect } from "react";
+
+const TypingEffect = ({
+  message,
+  speed,
+  avatar,
+  eraseSpeed = 50,
+  shouldErase = false,
+}) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [typing, setTyping] = useState(true);
+
+  useEffect(() => {
+    let timer;
+    if (typing) {
+      timer = setTimeout(() => {
+        setCurrentMessage(message.slice(0, currentMessage.length + 1));
+        if (currentMessage.length === message.length) {
+          setTyping(false);
+          if (shouldErase) {
+            setTimeout(() => {
+              setTyping(true);
+              setCurrentMessage("");
+            }, eraseSpeed);
+          }
+        }
+      }, speed);
+    }
+    return () => clearTimeout(timer);
+  }, [currentMessage, message, speed, typing, eraseSpeed, shouldErase]);
+
+  return (
+    <div className="typing-effect-container">
+      <div className="typing-effect-avatar">
+        <img src={avatar} alt="Avatar" />
+      </div>
+      <div className="typing-effect-arrow"></div>
+      <div className="typing-effect-message">
+        {currentMessage}
+        <span style={{ visibility: typing ? "visible" : "hidden" }}>|</span>
+      </div>
+    </div>
+  );
+};
+
+export default TypingEffect;
--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ import {
 
 import "./index.css";
 import Card from "./components/Card";
+import TypingEffect from "./components/TypingEffect";
 
 const Main = () => (
   <ReactiveBase
     app="movies-demo-app"
     url="https://a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61@appbase-demo-ansible-abxiydt-arc.searchbase.io"
-    reactivesearchAPIConfig={{
-      recordAnalytics: true,
-      userId: "jon",
+    theme={{
+      typography: {
+        fontFamily: "monospace",
+        fontSize: "16px",
+      },
     }}
     enableAppbase
     // endpoint={{
@@ -33,6 +36,16 @@ const Main = () => (
           searchboxId="q_and_a_search_ui"
           showClear
         />
+        <div className="ai-response-wrapper">
+          <TypingEffect
+            message={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in velit id velit ullamcorper ultricies. Ut fringilla, risus sit amet rhoncus bibendum, justo eros laoreet sapien, quis aliquet sapien nisl eu ex. Suspendisse vel lectus ante. Donec eget leo euismod, vehicula turpis sed, feugiat massa. Etiam auctor diam vitae justo sollicitudin, eu ultricies purus posuere. Etiam luctus lobortis nulla, eget mollis nulla varius eu. Nunc pulvinar sapien vel dui imperdiet bibendum. Aenean in lacus semper, volutpat velit eu, consequat elit. Duis malesuada massa justo, a lacinia arcu malesuada in. Nullam quis mauris sed odio pellentesque efficitur. Quisque molestie magna eu pharetra maximus. Mauris eu convallis odio. Nam scelerisque orci non efficitur hendrerit. Sed nec leo blandit, pretium risus in, vulputate eros."
+            }
+            speed={10}
+            eraseSpeed={20}
+            avatar={"https://www.svgrepo.com/show/361202/hubot.svg"}
+          />
+        </div>
         <br />
         <ReactiveList
           componentId="SearchResult"
@@ -46,7 +59,7 @@ const Main = () => (
           render={({ data }) => (
             <ReactiveList.ResultCardsWrapper>
               {data.map((item) => (
-                <Card {...item} />
+                <Card {...item} key={item._id} />
               ))}
             </ReactiveList.ResultCardsWrapper>
           )}
